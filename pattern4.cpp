6d4907fa43d49e9da9decb1d05dc1e95863ae80c
Shivanshu Prakash <shivanahuomili@gmail.com>
2021-07-14 22:17:40 +0530

    DSA in C++

---
+++ pattern4.cpp
@@ -0,0 +1,36 @@
+// //half pyramid using numbers 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for (int i = 1; i <=n; i++)
+//     {
+//         for (int j = 1; j<=i; j++)
+//         {
+//             cout<<i;
+//         }
+//        cout<<endl; 
+//     }
+    
+//     return 0;
+// }
+//pattern in series
+#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    int count=1;
+    cin>>n;
+    for (int i = 1; i <=n; i++)
+    {
+        for (int j =1; j <=i; j++)
+        {
+            cout<<count<<" ";
+            count++;
+        }
+       cout<<endl; 
+    }
+    
+    return 0;
+}
